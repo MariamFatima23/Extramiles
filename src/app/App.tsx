d4918fa01a310dfae398a2ba70de2a6572fa5aac
Mariam Fatima23 <mariamfatii78@gmail.com>
2026-09-24 12:21:38 -0700

    Set route-specific page titles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "../components/homePage/HomePage";
 import PartnerPage from "../components/partnerPage/PartnerPage";
@@ -5,6 +6,15 @@ import NotFoundPage from "../components/NotFoundPage";
 
 function AppRoutes() {
   const location = useLocation();
+
+  useEffect(() => {
+    const titles: Record<string, string> = {
+      "/home": "Home | ExtraMiles",
+      "/partner": "Partner | ExtraMiles",
+    };
+
+    document.title = titles[location.pathname] ?? "Page Not Found | ExtraMiles";
+  }, [location.pathname]);
 
   return (
     <div key={location.pathname} className="page-transition">
